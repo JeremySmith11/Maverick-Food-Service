4847347f7990ec52b9b27fe26c6691cc90d50ebc
JeremySmith <jeremysmith@unomaha.edu>
2019-02-01 11:35:31 -0600

    added home page with images

--- mfscrm/crm/views.py
+++ mfscrm/crm/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import redirect
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from .models import *
 from .forms import *
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 
 
 
+now = timezone.now()
+def home(request):
+   return render(request, 'crm/home.html',
+                 {'crm': home})
+
+@login_required
 def customer_list(request):
     customer = Customer.objects.filter(created_date__lte=timezone.now())
     return render(request, 'crm/customer_list.html',
                  {'customers': customer})
-
+@login_required
 def customer_edit(request, pk):
    customer = get_object_or_404(Customer, pk=pk)
    if request.method == "POST":
@@ -27,6 +35,7 @@ def customer_edit(request, pk):
        form = CustomerForm(instance=customer)
    return render(request, 'crm/customer_edit.html', {'form': form})
 
+@login_required
 def customer_delete(request, pk):
    customer = get_object_or_404(Customer, pk=pk)
    customer.delete()
